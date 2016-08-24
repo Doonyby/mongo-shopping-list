f4ae4d7ba4711b5232c50f79e39ce7e1cf4204a9
danriding <doonyby@gmail.com>
2016-08-24 22:24:16 +0000

    heroku travis deployment

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
+                            'mongodb://<doonyby@gmail.com>:<Doonyby21>@ds013956.mlab.com:13956/mongo-shopping-list' :
                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
 
